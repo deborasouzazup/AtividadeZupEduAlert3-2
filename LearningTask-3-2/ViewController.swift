a13a7f7ea033f077dd53cfd2f67d9a3b88759f74
Debora Rodrigues <deboral.p@hotmail.com>
2022-08-17 20:08:46 -0300

    Add alert

--- LearningTask-3-2/ViewController.swift
+++ LearningTask-3-2/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func BotãoConfirmarTRanferencia(_ sender: Any) {
+        exibirAlertaDeTransacao()
+    }
+    
+    func exibirAlertaDeTransacao() {
+        let alert = UIAlertController(title: "Tudo pronto", message: "Estamos verificando a transação.                      Em instantes você deve receber uma notificação com a confirmação da transferência.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
